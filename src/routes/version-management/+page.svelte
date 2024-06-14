78b88dd076e7b9e263cf0287824973e92ae6fb66
Afrody <herdianafrody@gmail.com>
2024-06-14 13:31:22 +0700

    Fix tampilan terminal

--- src/routes/version-management/+page.svelte
+++ src/routes/version-management/+page.svelte
@@ -13,8 +13,8 @@
 		db: 'Active database',
 		ip: 'IP Address'
 	};
-	let connectedServer = {};
-	let searchQuery = '';
+	let connectedServer = [];
+	// let searchQuery = '';
 	let filteredData = $hospitals;
 
 	const checkRole = () => {
@@ -57,7 +57,7 @@
 
 	const disconnectFromServer = () => {
 		if (confirm('Anda ingin memutus koneksi dengan server ini?')) {
-			connectedServer = {};
+			connectedServer = [];
 		}
 	};
 
@@ -261,11 +261,15 @@
 	{/if}
 	<!-- End of Detail Server Bar -->
 
-	<div class="fixed bottom-0 w-7/12">
-		<textarea
-			name="test"
-			id="test"
-			class="w-full h-20 bg-gray-900 p-1 resize-none text-gray-200 rounded">Test</textarea
-		>
-	</div>
+	{#if connectedServer.length !== 0}
+		<div class="fixed bottom-0 w-7/12">
+			<!-- <textarea
+				name="test"
+				id="test"
+				class="w-full h-20 bg-gray-900 p-1 resize-none text-gray-200 rounded">Test</textarea
+			> -->
+			<pre
+				class="w-full h-20 bg-gray-800 p-1 text-gray-200 text-wrap rounded overflow-y-auto">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui aspernatur magnam laborum facere ut quae vel maxime eaque vero beatae, atque quibusdam a aliquid nesciunt saepe inventore dolorum delectus alias dolor dolores cum? Molestias explicabo facilis tenetur doloremque architecto optio dolore error ea, reiciendis repellendus accusamus recusandae expedita aspernatur dolor eius nobis asperiores quae minima totam repellat vel! Incidunt veniam repudiandae repellendus eaque nobis. Deleniti repudiandae dolor soluta distinctio quas voluptatem esse voluptatum corrupti eligendi, tempore quis at nihil repellendus neque accusantium reiciendis eos amet obcaecati harum sunt nisi voluptatibus libero ipsa. Doloribus porro blanditiis natus, nulla sapiente, aperiam ipsum ratione accusantium sit quisquam laboriosam assumenda culpa fuga reiciendis dolor eius dolorem ut quis dolore. Quaerat tempora ad quasi necessitatibus a iste quo at deserunt? Consequatur soluta nobis sint atque excepturi neque ipsa dolor corporis dolorum doloribus quas in, dolorem quod qui dolore, aspernatur necessitatibus nisi fuga totam accusantium dignissimos. Nulla delectus blanditiis pariatur, magni recusandae atque, modi, nisi laboriosam nihil inventore quibusdam reprehenderit. Mollitia nemo quia soluta sunt, odio aperiam saepe reprehenderit maiores quo quidem nihil blanditiis quos sapiente corporis molestiae veniam quas? Perferendis sed beatae amet commodi dolorum molestias reiciendis ipsa! Quaerat quo laboriosam molestiae nemo, explicabo commodi?</pre>
+		</div>
+	{/if}
 </div>
